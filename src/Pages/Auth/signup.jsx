466ce04433bd8e35c7802c69c6c23498967372a7
Khaled mohamed <Khal2002>
2026-02-08 16:40:48 +0200

    handel sign up page

--- src/Pages/Auth/signup.jsx
+++ src/Pages/Auth/signup.jsx
@@ -7,36 +7,78 @@ import { Controller } from "react-hook-form";
 import { Alert } from "@heroui/alert";
 import { useState } from "react";
 import { Input } from "@heroui/react";
-
+import axios from "axios";
 import { Button } from "@heroui/react";
-import { p } from "framer-motion/client";
 export default function signup() {
   const [sucess, setsucsess] = useState(false);
   const [faildata, setfail] = useState(false);
   const [loading, setloading] = useState(false);
-  const signupschema = z.object({
-    name: z
-      .string()
-      .nonempty("name is requiered")
-      .min(2, "please enter first and socend name"),
-    email: z
-      .string()
-      .nonempty("email is requiered")
-      .email("please enter valid email"),
-    password: z
-      .string()
-      .nonempty("please enter password")
-      .min(10, "enter at least 10 string"),
-    rePassword: z.string().nonempty("please re enter password"),
-  });
+
+  const signupschema = z
+    .object({
+      name: z
+        .string()
+        .nonempty("name is requiered")
+        .min(2, "please enter first and socend name"),
+      email: z
+        .string()
+        .nonempty("email is requiered")
+        .email("please enter valid email"),
+      password: z
+        .string()
+        .nonempty("please enter password")
+        .min(10, "enter at least 10 string"),
+      rePassword: z.string().nonempty("please re enter password"),
+      gender: z.string().nonempty("please choose your gender"),
+      dateOfBirth: z
+        .string()
+        .nonempty("please enter your birth date ")
+
+        .refine(
+          (birthdate) => {
+            const userDate = new Date(birthdate);
+            const currentDate = new Date();
+            return currentDate.getFullYear() - userDate.getFullYear() >= 18;
+          },
+          {
+            message: "your age must be >=18 ",
+          },
+        ),
+    })
+    .refine((data) => data.password === data.rePassword, {
+      message: "repassword not equal password ",
+      path: ["rePassword"],
+    });
   let {
     register,
     handleSubmit,
     control,
     formState: { errors },
-  } = useForm();
-  function handelsignup(data) {
-    console.log(data);
+  } = useForm({
+    resolver: zodResolver(signupschema),
+    mode: "onChange",
+    defaultValues: {
+      gender: "",
+    },
+  });
+  async function handelsignup(dataForm) {
+    try {
+      setloading(true);
+      const { data } = await axios.post(
+        "https://linked-posts.routemisr.com/users/signup",
+        dataForm,
+        { headers: { "Content-Type": "application/json" } },
+      );
+      setfail(false);
+      setsucsess(true);
+      setloading(false);
+      console.log(data);
+    } catch (error) {
+      setfail(true);
+      setsucsess(false);
+      setloading(false);
+      console.log(error);
+    }
   }
 
   return (
@@ -56,7 +98,7 @@ export default function signup() {
             <Alert className="bg-success text-white">login successfuly</Alert>
           ) : null}
 
-          <div className="text-5xl text-center ">Login</div>
+          <div className="text-5xl text-center ">Sign Up</div>
           <div className="main_form p-5 flex flex-col gap-4">
             <Input
               label="name"
@@ -95,17 +137,35 @@ export default function signup() {
             {errors.rePassword && (
               <p className="text-red-500">{errors.rePassword.message}</p>
             )}
+            <Input
+              {...register("dateOfBirth")}
+              variant="underlined"
+              label={"Birthdate"}
+              type="date"
+            ></Input>
+            {errors.dateOfBirth && (
+              <p className="text-red-500">{errors.dateOfBirth.message}</p>
+            )}
 
             <Controller
               name="gender"
               control={control}
               render={({ field }) => (
-                <RadioGroup {...field} label={"gender"}>
+                <RadioGroup
+                  {...field}
+                  label={"gender"}
+                  value={field.value}
+                  onValueChange={field.onChange}
+                >
                   <Radio value={"male"}>male</Radio>
                   <Radio value={"female"}>female</Radio>
                 </RadioGroup>
               )}
             />
+            {errors.gender && (
+              <p className="text-red-500"> {errors.gender.message} </p>
+            )}
+
             <Button
               isLoading={loading}
               type="submit"
